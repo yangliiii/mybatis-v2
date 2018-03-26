11900ec51d50b8f894ef83c39bdaf5dff5e3b590
yangliiii <31989509+yangliiii@users.noreply.github.com>
2018-03-26 14:37:49 +0800

    mybatis-v2

---
+++ src/test/java/com/yangli/mybatis/lombok/ELombok.java
@@ -0,0 +1,16 @@
+package com.yangli.mybatis.lombok;
+
+import lombok.Data;
+import lombok.Setter;
+
+/**
+ * Created by lies on 2018/3/26.
+ */
+@Setter
+public class ELombok {
+
+    private String username;
+    private String pwd;
+    private String nickname;
+    private String addr;
+}
---
+++ src/test/java/com/yangli/mybatis/lombok/TestLombok.java
@@ -0,0 +1,11 @@
+package com.yangli.mybatis.lombok;
+
+/**
+ * Created by lies on 2018/3/26.
+ */
+public class TestLombok {
+    public static void main(String[] args) {
+        ELombok eLombok = new ELombok();
+        eLombok.setAddr("");
+    }
+}
